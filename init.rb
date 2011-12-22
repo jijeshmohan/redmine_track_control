ed207b092fe804a87af070af391670921eb87481
Jijesh Mohan <jijeshmohan@gmail.com>
2011-12-22 22:04:57 +0530

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_track_control do
+  name 'Redmine Tracker Control plugin'
+  author 'Jijesh Mohan'
+  description 'Plugin for controlling tracker wise issue creation'
+  version '1.0.0'
+  url 'i'
+  author_url ''
+end
