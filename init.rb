e03077dc517ca85b0aaf89204d87f02e3c460c61
Jijesh Mohan <jijeshmohan@gmail.com>
2012-01-19 22:39:58 +0530

    fixed unicode tracker issue

--- init.rb
+++ init.rb
@@ -5,15 +5,18 @@ require 'dispatcher'
 Dispatcher.to_prepare :redmine_track_control do
   require_dependency 'tracker'
   Tracker.send(:include, TrackerPatch) unless Issue.included_modules.include? TrackerPatch
+
+  require_dependency 'role'
+  Role.send(:include, RolePatch)
 end
 
 Redmine::Plugin.register :redmine_track_control do
   name 'Redmine Tracker Control plugin'
   author 'Jijesh Mohan'
   description 'Plugin for controlling tracker wise issue creation'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/jijeshmohan/redmine_track_control'
-  author_url ''
+  author_url 'jijeshmohan.wordpress.com'
 
   project_module :tracker_permissions do
     Tracker.all.each do |t|
