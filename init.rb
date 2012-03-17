cc40083935624097eadca4c59645db4ff80b6ce3
Jijesh Mohan <jijeshmohan@gmail.com>
2012-03-17 22:31:20 +0530

    fixed issue #3

--- init.rb
+++ init.rb
@@ -8,13 +8,16 @@ Dispatcher.to_prepare :redmine_track_control do
 
   require_dependency 'role'
   Role.send(:include, RolePatch)
+
+  require_dependency 'issue'
+  Issue.send(:include, IssuePatch) unless  Issue.included_modules.include? IssuePatch
 end
 
 Redmine::Plugin.register :redmine_track_control do
   name 'Redmine Tracker Control plugin'
   author 'Jijesh Mohan'
   description 'Plugin for controlling tracker wise issue creation'
-  version '1.0.1'
+  version '1.0.2'
   url 'https://github.com/jijeshmohan/redmine_track_control'
   author_url 'jijeshmohan.wordpress.com'
 
---
+++ lib/issue_patch.rb
@@ -0,0 +1,17 @@
+module  IssuePatch
+  def self.included(base)
+    base.send(:include, InstanceMethods)
+    base.class_eval do
+          unloadable
+          validate :is_valid_tracker
+    end
+  end
+
+  module InstanceMethods
+    private
+      def is_valid_tracker
+        tracker_name = "create_#{self.tracker.name}_tracker".to_sym
+        errors.add(:tracker_id,"not allowed") if User.current.allowed_to?(tracker_name, self.project, :global => true).nil?
+      end
+  end
+end
