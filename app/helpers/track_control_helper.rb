56ab531274a6524659335ab624a479ffb11ec1cf
Jijesh Mohan <jijeshmohan@gmail.com>
2013-04-29 12:27:54 +0530

    Fixed #14

--- app/helpers/track_control_helper.rb
+++ app/helpers/track_control_helper.rb
@@ -1,10 +1,14 @@
  module TrackControlHelper
     def display_tracker_select(project,issue,f)
-       tracker_list=project.trackers.select{|t| User.current.allowed_to?("create_#{t.name.downcase.gsub(/\ +/,'_')}_tracker".to_sym, project, :global => true)}.collect {|t| [t.name, t.id]}
+      if project.enabled_modules.where(:name => "tracker_permissions").count == 1
+         tracker_list=project.trackers.select{|t| User.current.allowed_to?("create_#{t.name.downcase.gsub(/\ +/,'_')}_tracker".to_sym, project, :global => true)}.collect {|t| [t.name, t.id]}
         unless issue.new_record?
           current_tracker = [issue.tracker.name,issue.tracker.id]
           tracker_list << current_tracker unless current_tracker.include? current_tracker
         end
+      else
+        tracker_list = project.trackers.collect {|t| [t.name, t.id]}
+      end
          f.select :tracker_id, tracker_list, {:required => true},
                   :onchange => "updateIssueFrom('#{escape_javascript project_issue_form_path(project, :id => issue, :format => 'js')}')"
       end
--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ Redmine::Plugin.register :redmine_track_control do
   name 'Redmine Tracker Control plugin'
   author 'Jijesh Mohan'
   description 'Plugin for controlling tracker wise issue creation'
-  version '1.0.7'
+  version '1.0.8'
   url 'https://github.com/jijeshmohan/redmine_track_control'
   author_url 'jijeshmohan.wordpress.com'
 
