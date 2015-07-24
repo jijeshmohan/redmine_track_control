b24211fa17a3e66a793d0463bbd7ca6e2da05bcb
fishermans <zu.mir@xyz.de>
2015-07-24 16:53:12 +0200

    * Fixed: NoMethodError (undefined method `enabled_modules' for nil:NilClass)

--- lib/redmine_track_control/tracker_helper.rb
+++ lib/redmine_track_control/tracker_helper.rb
@@ -16,10 +16,12 @@ module RedmineTrackControl
 
     # Gets the list of valid trackers for the project for the current user
     def self.valid_trackers_list(project)
-      if project.enabled_modules.where(:name => "tracker_permissions").count == 1
-        project.trackers.select{|t| User.current.allowed_to?(permission(t), project, :global => true)}.collect {|t| [t.name, t.id]}
-      else
-        project.trackers.collect {|t| [t.name, t.id]}
+      if not project.nil?
+        if project.enabled_modules.where(:name => "tracker_permissions").count == 1
+          project.trackers.select{|t| User.current.allowed_to?(permission(t), project, :global => true)}.collect {|t| [t.name, t.id]}
+        else
+          project.trackers.collect {|t| [t.name, t.id]}
+        end
       end
     end
   end
